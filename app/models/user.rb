bcf56510a74d4c885f82672f3ef9d29baf3e207d
mopuriiswaryalakshmi <milakshmi33@gmail.com>
2017-10-31 13:06:57 +0530

    devise setting

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+   has_many :movies
+
+   has_attached_file :image, styles: { medium: "300x300>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+end
