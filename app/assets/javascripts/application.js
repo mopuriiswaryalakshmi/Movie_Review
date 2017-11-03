3dd3f26ef979187ca7b5e28c7730eb0e09a751df
mopuriiswaryalakshmi <milakshmi33@gmail.com>
2017-11-03 15:24:29 +0530

    jquery gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
